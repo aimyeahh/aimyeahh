44ad2391a33ce85da547e386d9f1e44fb67e7584
Sunti <saknsaza87@gmail.com>
2023-03-11 12:54:23 +0700

    update

---
+++ reading/js/timer.js
@@ -0,0 +1,42 @@
+
+
+let interval;
+let check = Number(localStorage.getItem('total'))
+let total = 00
+if( check > 0){
+    interval = setInterval(Timer, 1000);
+}
+function totalValue(){
+    let hour = Number(localStorage.getItem('hour'))
+    let minute = Number(localStorage.getItem('minute'))
+    let second =  Number(localStorage.getItem('second'))
+    let total_count = hour * 3600 + minute * 60 + second
+    localStorage.setItem('total',total_count );
+    total = localStorage.getItem('total')
+}
+function Timer () {
+    totalValue()
+    total--;
+    console.log("total" , total)
+    localStorage.setItem('total', total)
+    if (total >= 00) {
+        var hr = Math.floor(total / 3600);
+        var mt = Math.floor((total / 60) - (hr * 60));
+        var sc = total - ((hr * 3600) + (mt * 60));
+
+        localStorage.setItem('hour',  hr)
+        localStorage.setItem('minute', mt)
+        localStorage.setItem('second', sc)
+        console.log("hour" , hr)
+        console.log("minute" , mt)
+        console.log("second" , sc)
+    }
+    else {
+
+        let sound = new Audio('sound.mp3');
+        sound.play();
+        clearInterval(interval);
+        alert('Time Over!!!')
+
+    }
+}
--- reading/script.js
+++ reading/script.js
@@ -1,6 +1,9 @@
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
+localStorage.setItem('hour', hour)
+localStorage.setItem('minute', minute)
+localStorage.setItem('second', second)
 
 const start = document.getElementById('start');
 const reset = document.getElementById('reset');
@@ -9,43 +12,48 @@ const disp = document.getElementById('disp');
 
 var interval = null;
 var total = 00;
+localStorage.setItem('total', total)
 
-totalValue = ()=>{
-    total = Number(hour.value)*3600 + Number(minute.value)*60 + Number(second.value);
+totalValue = () => {
+    total = Number(hour.value) * 3600 + Number(minute.value) * 60 + Number(second.value);
+    localStorage.setItem('total', total)
 }
 
-Timer = ()=>{
-    totalValue(); 
+Timer = () => {
+    totalValue();
     total--;
+    console.log(total)
+    localStorage.setItem('total', total)
+    if (total >= 00) {
+        var hr = Math.floor(total / 3600);
+        var mt = Math.floor((total / 60) - (hr * 60));
+        var sc = total - ((hr * 3600) + (mt * 60));
 
-    if(total >= 00)
-    {
-       var hr = Math.floor(total/3600);
-       var mt = Math.floor((total/60) - (hr*60));
-       var sc = total - ((hr*3600) + (mt*60));
-
-       hour.value = hr;
-       minute.value = mt;
-       second.value = sc;
+        hour.value = hr;
+        minute.value = mt;
+        second.value = sc;
+        localStorage.setItem('hour',  hour.value)
+        localStorage.setItem('minute', minute.value)
+        localStorage.setItem('second', second.value)
     }
-    else{
+    else {
 
         let sound = new Audio('sound.mp3');
         sound.play();
         clearInterval(interval);
         disp.innerText = "Time Over!!!";
-        
+
     }
 }
 
-start.addEventListener('click', ()=>{
+start.addEventListener('click', () => {
     clearInterval(interval);
     interval = setInterval(Timer, 1000);
 
     disp.innerText = "Timer Started";
 });
 
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
 
     clearInterval(interval);
 
